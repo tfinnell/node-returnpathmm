2b486e773414d6fe851c6dbc616aee73ee0789e9
Felix Hoffmann <f.hoffman@gmail.com>
2012-03-16 17:55:36 -0700

    returns seedlist

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var xmlrpc   = require('xmlrpc'),
-    xmlcl    = xmlrpc.createSecureClient('https://api.returnpath.net/mmapi'),
-    email    = process.argv[2],
-    password = process.argv[3];
+var xmlrpc = require('xmlrpc'),
+    xmlcl  = xmlrpc.createSecureClient('https://api.returnpath.net/mmapi'),
+    opts   = {
+      'email': process.argv[2],
+      'password': process.argv[3]
+    };
 
-xmlcl.methodCall('seedlist', {
-  email: email, password: password
-}, function (err, val) {
-  console.log(val);
+xmlcl.methodCall('RP.API.MM.seedList', [opts], function (err, val) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(val);
+  }
 });
